4d6cc5fa38c7233ba129016037ece0359c04f33d
Ly Nguyen <Ly900@aol.com>
2020-02-20 21:52:56 -0500

    change all info to cat-related

--- src/components/compare-top/compare-top.js
+++ src/components/compare-top/compare-top.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import '../compare-top/compare-top.scss';
 import '../../utils/utils.scss';
-import nRewardsImg from '../../images/nrewards.svg';
-import cashRewardsImg from '../../images/cashrewards.svg';
-import amexImg from '../../images/amex.svg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 
@@ -16,17 +13,17 @@ function CompareTop() {
 					<div className="compare-top__item" data-cardid="cashrewards" role="listitem">
 						<div className="compare-top__item-wrapper">
 							<figure className="compare-top__item-img-wrapper">
-								<img alt="" className="compare-top__item-img" src={cashRewardsImg} />
+								<img alt="" className="compare-top__item-img" src="http://placekitten.com/496/312" />
 							</figure>
 
-							<a className="compare-top__item-link" href="/loans-cards/credit-cards/cash-rewards/">
-								<span className="compare-top__item-title"><em>cash</em>Rewards</span>
+							<a className="compare-top__item-link" href="cat1.html">
+								<span className="compare-top__item-title">Cat No. 1</span>
 							</a>
 							<div className="compare-top__item-ctas">
-								<a href="https://myaccounts.navyfederal.org/NFCU/accountservices/ifse?id=creditcardapplication&amp;ProductID=NFCC01" className="compare-top__item-link compare-top__item-apply-button btn btn_center">Apply Now <span className="sr-only">for the <em>cash</em>Rewards card.</span></a>
-								<a href="/loans-cards/credit-cards/cash-rewards/" className="compare-top__item-link compare-top__item-details-link">Card Details <span className="sr-only">for the <em>cash</em>Rewards card.</span></a>
+								<a href="cat1/adopt.html" className="compare-top__item-link compare-top__item-apply-button btn btn_center">Adopt Now<span className="sr-only"> Cat No. 1</span></a>
+								<a href="cat1.html" className="compare-top__item-link compare-top__item-details-link">Details<span className="sr-only">for Cat No. 1</span></a>
 							</div>
-							<button className="compare-top__item-close-button" aria-label="Remove the <em>cash</em>Rewards Credit Card from the Compare Results page">
+							<button className="compare-top__item-close-button" aria-label="Remove the Cat No. 1 from the Compare Results page">
 								{/* <i className="icon-close-solid" aria-hidden="true"></i> */}
 								<FontAwesomeIcon icon={faTimesCircle} />
 							</button>
@@ -36,17 +33,17 @@ function CompareTop() {
 					<div className="compare-top__item" data-cardid="morerewards" role="listitem">
 						<div className="compare-top__item-wrapper">
 							<figure className="compare-top__item-img-wrapper">
-								<img alt="" className="compare-top__item-img" src={amexImg} />
+								<img alt="" className="compare-top__item-img" src="http://placekitten.com/g/496/312" />
 							</figure>
 
-							<a className="compare-top__item-link" href="/loans-cards/credit-cards/more-rewards/">
-								<span className="compare-top__item-title">More Rewards</span>
+							<a className="compare-top__item-link" href="cat2.html">
+								<span className="compare-top__item-title">Cat No. 2</span>
 							</a>
 							<div className="compare-top__item-ctas">
-								<a href="https://myaccounts.navyfederal.org/NFCU/accountservices/ifse?id=creditcardapplication&amp;ProductID=NFCC07" className="compare-top__item-link compare-top__item-apply-button btn btn_center">Apply Now <span className="sr-only">for the More Rewards card.</span></a>
-								<a href="/loans-cards/credit-cards/more-rewards/" className="compare-top__item-link compare-top__item-details-link">Card Details <span className="sr-only">for the More Rewards card.</span></a>
+								<a href="cat2/adopt.html" className="compare-top__item-link compare-top__item-apply-button btn btn_center">Adopt Now<span className="sr-only"> Cat No. 2</span></a>
+								<a href="cat2.html" className="compare-top__item-link compare-top__item-details-link">Details<span className="sr-only">for Cat No. 2</span></a>
 							</div>
-							<button className="compare-top__item-close-button" aria-label="Remove the <em>cash</em>Rewards Credit Card from the Compare Results page">
+							<button className="compare-top__item-close-button" aria-label="Remove the Cat No. 2 from the Compare Results page">
 								{/* <i className="icon-close-solid" aria-hidden="true"></i> */}
 								<FontAwesomeIcon icon={faTimesCircle} />
 							</button>
@@ -56,17 +53,17 @@ function CompareTop() {
 					<div className="compare-top__item" data-cardid="nrewards" role="listitem">
 						<div className="compare-top__item-wrapper">
 							<figure className="compare-top__item-img-wrapper">
-								<img alt="" className="compare-top__item-img" src={nRewardsImg} />
+								<img alt="" className="compare-top__item-img" src="http://placebear.com/496/312" />
 							</figure>
 
-							<a className="compare-top__item-link" href="/loans-cards/credit-cards/nrewards-secured/">
-								<span className="compare-top__item-title"><em>n</em>Rewards</span>
+							<a className="compare-top__item-link" href="bear.html">
+								<span className="compare-top__item-title">Bear No. 1</span>
 							</a>
 							<div className="compare-top__item-ctas">
-								<a href="#nrewards-modal" className="compare-top__item-link compare-top__item-apply-button btn btn_center original-nrewards-link" data-href="https://myaccounts.navyfederal.org/NFCU/accountservices/ifse?id=creditcardapplication&amp;ProductID=NFCC03" data-modal="true">Apply Now <span className="sr-only">for the <em>n</em>Rewards® Secured card.</span></a>
-								<a href="/loans-cards/credit-cards/nrewards-secured/" className="compare-top__item-link compare-top__item-details-link">Card Details <span className="sr-only">for the <em>n</em>Rewards® Secured card.</span></a>
+								<a href="bear1/adopt.html" className="compare-top__item-link compare-top__item-apply-button btn btn_center">Adopt Now<span className="sr-only"> Bear No. 1</span></a>
+								<a href="bear1.html" className="compare-top__item-link compare-top__item-details-link">Details<span className="sr-only"> for the Bear</span></a>
 							</div>
-							<button className="compare-top__item-close-button" aria-label="Remove the <em>cash</em>Rewards Credit Card from the Compare Results page">
+							<button className="compare-top__item-close-button" aria-label="Remove the Bear from the Compare Results page">
 								{/* <i className="icon-close-solid" aria-hidden="true"></i> */}
 								<FontAwesomeIcon icon={faTimesCircle} />
 							</button>
