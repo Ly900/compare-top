792c3303198dcf7b5595cb68be81d84868ea1c85
Ly Nguyen <Ly900@aol.com>
2020-02-23 09:24:01 -0500

    dynamically care compare modal inputs based on json data

--- src/components/compare-results-page/compare-modal/compare-modal.js
+++ src/components/compare-results-page/compare-modal/compare-modal.js
@@ -3,16 +3,66 @@ import React from 'react';
 // import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 // import '../../compare-top/compare-top.scss';
 // import blankCard from './blank-card.svg';
+import JSONData from '../../../cats.json';
 
-const CompareModal = (props) => {
+class CompareModal extends React.Component {
 
-	return (
+	constructor(props) {
+		super(props);
+		this.state = {
+			loading: true,
+			checkBoxItems: null
+		}
+	}
 
-		<div>
-			<h1>I am the Compare Modal.</h1>
-		</div>
+	// getOptionsFromJSON() {
+	// 	return options = JSONData.map((option) => {
+	// 		// console.log("option: ", option);
+	// 	})
+	// }
 
-	)
+	componentDidMount() {
+		console.log("component mounted");
+		const options = JSONData.map((option) => {
+			return option;
+		})
+		this.setState(() => ({
+			loading: false,
+			checkBoxItems: options
+		}))
+	}
+
+	onSubmit(e) {
+		e.preventDefault();
+		console.log("form submitted");
+		console.log(e.target);
+	}
+
+	render() {
+
+		if (this.state.loading) {
+			return <div>Loading...</div>;
+		}
+
+		return (
+
+			<form onSubmit={this.onSubmit}>
+				{
+					this.state.checkBoxItems.map((item, index) => {
+						return (
+							<div key={item.productId}>
+								<input type="checkbox" name={item.productName} value={item.productName} />
+								<label >Add {item.productName} to Compare</label>
+							</div>
+						)
+					})
+				}
+				<input type="submit" />
+			</form>
+
+		)
+	}
+
 
 }
 
