925f94676121c0c8725ab7af0afa7257178699bc
Ly Nguyen <Ly900@aol.com>
2020-02-23 10:40:39 -0500

    add and remove options from array

--- src/components/compare-results-page/compare-modal/compare-modal.js
+++ src/components/compare-results-page/compare-modal/compare-modal.js
@@ -11,14 +11,34 @@ class CompareModal extends React.Component {
 		super(props);
 		this.state = {
 			loading: true,
-			checkBoxItems: null
+			selectedItemIds: []
 		}
+	}
+
+	checkItem(e) {
+		console.log("item checked");
+		console.log(e.target);
+		let updatedIdsList;
+		// If the checkbox was not already selected, add it to the array.
+		if (this.state.selectedItemIds.indexOf(e.target.id) === -1) {
+			updatedIdsList = this.state.selectedItemIds.concat(e.target.id);
+			// If the checkbox was already selected, remove it from the array.
+		} else {
+			updatedIdsList = this.state.selectedItemIds.filter((item) => {
+				return item !== e.target.id;
+			})
+		}
+		this.setState(() => ({
+			selectedItemIds: updatedIdsList
+		}))
 	}
 
 	onSubmit(e) {
 		e.preventDefault();
 		console.log("form submitted");
 		console.log(e.target);
+		// console.log("selected item ids: ", this.state.selectedItemIds);
+
 	}
 
 	render() {
@@ -34,11 +54,23 @@ class CompareModal extends React.Component {
 					this.props.checkBoxItems.map((item, index) => {
 						return (
 							<div key={item.productId}>
-								<input type="checkbox" name={item.productName} value={item.productName} />
+								<input type="checkbox"
+									id={item.productId}
+									name={item.productName}
+									value={item.productName}
+									onChange={(e) => this.checkItem(e)} />
 								<label >Add {item.productName} to Compare</label>
 							</div>
 						)
 					})
+
+				}
+				{
+
+					this.state.selectedItemIds.map((item) => {
+						return <p key={item}>{item}</p>
+					})
+
 				}
 				<input type="submit" />
 			</form>
